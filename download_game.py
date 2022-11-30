d69cbafcf95adee0bc89fc92a4344e17dff6a0db
freya <111>
2022-11-30 23:25:22 +0800

    V0.10.0

--- download_game.py
+++ download_game.py
@@ -1,19 +1,27 @@
 import requests
 import argparse
 from tqdm import tqdm
-import py7zr
+from zipfile import ZipFile
+from datetime import datetime
 import os
 
 
-def download(download_url, download_file):
-    url = download_url
-    print(url)
-    response = requests.get(url, stream=True)    # 设stream流传输方式为真
+def download(download_file):
+    print("开始从阿里云下载游戏")
+    print(aliyun_download_url)
+    response = requests.get(aliyun_download_url, stream=True)  # 设stream流传输方式为真
+    if response.headers["Server"] != "AliyunOSS":
+        print("开始从百度下载游戏")
+        headers = {
+            "User-Agent": "pan.baidu.com"
+        }
+        response = requests.get(baidu_download_url, headers=headers, stream=True)  # 设stream流传输方式为真
+
     # print(response.headers)               # 打印查看基本头信息
     data_size = int(response.headers['Content-Length'])/1024/1024                   # 字节/1024/1024=MB
     # print(data_size/100)
     with open(rf'{download_file}', 'wb') as f:
-        for data in tqdm(iterable=response.iter_content(1024*1024), total=data_size, desc='正在下载', unit='MB'):
+        for data in tqdm(iterable=response.iter_content(1024*1024), total=round(data_size), desc='正在下载', unit='MB'):
             f.write(data)
 
 
@@ -26,22 +34,56 @@ if __name__ == '__main__':
     # 加工变量
     gamedbid = args.game_dbid
     playnitedir = args.PlayniteDir
-    download_temp_dir = rf"{playnitedir}\CustomGames(flyklmwl)\temp"
-    download_file = rf"{download_temp_dir}\{gamedbid}.7z"
-    gamesource_dir = rf"{playnitedir}\CustomGames(flyklmwl)\gamedir_source\PC"
-    aliyun_url = "http://alist.windcs.cn/d/gamedir_source/PC"
-    download_url = f"{aliyun_url}/{gamedbid}.7z"
+    download_temp_dir = rf"{playnitedir}\..\Playnite_Games\temp"
+    download_file = rf"{download_temp_dir}\{gamedbid}.zip"
+    gamesource_dir = rf"{playnitedir}\..\Playnite_Games\PC"
+    # aliyun_url = "http://alist.windcs.cn/d/gamedir_source/PC"
+    aliyun_url = "http://alist.windcs.cn/d/aliyun/gamedir_source/PC"
+    baidu_url = "http://alist.windcs.cn/d/baidu/gamedir_source/PC"
+    aliyun_download_url = f"{aliyun_url}/{gamedbid}.zip"
+    baidu_download_url = f"{baidu_url}/{gamedbid}.zip"
+    game_dir = rf"{gamesource_dir}\{gamedbid}"
 
     # 判断是否存在游戏目录
-    if os.path.exists(rf"{gamesource_dir}\{gamedbid}"):
+    if os.path.exists(game_dir):
         print("游戏已经安装")
         os.system("pause")
         exit(-1)
     # 下载到temp目录
-    download(download_url, download_file)
+    try:
+        download(download_file)
+    except FileNotFoundError:
+        os.makedirs(download_temp_dir)
+        download(download_file)
     # 解压到gamedir
-    with py7zr.SevenZipFile(download_file, 'r') as archive:
-        archive.extractall(gamesource_dir)
+    start_time = datetime.now()
+    with ZipFile(file=download_file) as zip_file:
+        for file in tqdm(iterable=zip_file.namelist(), total=len(zip_file.namelist())):
+            try:
+                zip_file.extract(member=file, path=gamesource_dir)
+            except:
+                zip_file.extract(member=file, path=gamesource_dir, pwd="Flykl3321*")
+
+    # 解压完成后处理乱码
+    for root, dirs, files in os.walk(game_dir):
+        for d in dirs:
+            try:
+                new_dname = d.encode('cp437').decode('gbk')
+                os.rename(os.path.join(root, d), os.path.join(root, new_dname))
+            except:
+                new_dname = d.encode('cp437').decode('utf-8')
+                os.rename(os.path.join(root, d), os.path.join(root, new_dname))
+    for root, dirs, files in os.walk(game_dir):
+        for f in files:
+            try:
+                new_name = f.encode('cp437').decode('gbk')
+                os.rename(os.path.join(root, f), os.path.join(root, new_name))
+            except:
+                new_name = f.encode('cp437').decode('utf-8')
+                os.rename(os.path.join(root, f), os.path.join(root, new_name))
+    end_time = datetime.now()
+    use_time = end_time - start_time
+    print(f"Usetime is {use_time}")
     os.remove(download_file)
-    print("已安装完毕")
+    print("游戏已安装完毕")
     os.system("pause")
--- parse_data.py
+++ parse_data.py
@@ -1,5 +1,6 @@
 import json
 import re
+import os
 
 
 def addnewoption():
@@ -36,22 +37,83 @@ def modify_save_gui_path():
         items = json.load(json_file)
     modify_count = 0
     for item in items:
+        # 把备份存档的路径替换
+        # try:
+        #     if item['GameActions']:
+        #         for action in item['GameActions']:
+        #             if action['Name'] == "备份存档":
+        #                 modify_count += 1
+        #                 # print(action['Path'])
+        #                 if action['Path'] == "{PlayniteDir}\save_gui\save_gui.exe":
+        #                     path = re.sub(r"\{PlayniteDir\}", r"{PlayniteDir}\\CustomGames(flyklmwl)", action['Path'])
+        #                     action['Path'] = path
+        #                     print(action['Path'])
+        # except KeyError:
+        #     pass
         try:
-            if item['GameActions']:
-                for action in item['GameActions']:
-                    if action['Name'] == "备份存档":
-                        modify_count += 1
-                        # print(action['Path'])
-                        if action['Path'] == "{PlayniteDir}\save_gui\save_gui.exe":
-                            path = re.sub(r"\{PlayniteDir\}", r"{PlayniteDir}\\CustomGames(flyklmwl)", action['Path'])
-                            action['Path'] = path
-                            print(action['Path'])
+            # installdir = re.sub(r"\.\.\\Playnite_Games\\PC", r".\\CustomGames\(flyklmwl\)\\gamedir_source\\PC", item["InstallDirectory"])
+            print(item["InstallDirectory"])
+            modify_count += 1
         except KeyError:
             pass
+    print(f"总计installdir有{modify_count}项")
+
     with open("data/games_new.json", "w+", encoding='utf-8') as f:
         json.dump(items, f, ensure_ascii=False)
+
+
+def check_cloud_games():
+    # GameId 是游戏id，并不是数据库id(_id才是)，游戏id是可能为空的
+    game_list = []
+    playnite_list = []
+    playnite_list_id = []
+    with open('data/games.json', 'r') as json_file:
+        items = json.load(json_file)
+    count = 0
+    pc_games = os.listdir("e:\gamedir_source\PC")
+    print(type(pc_games))
+    for item in items:
+        # if item['_id']["$guid"] == "afec845b-6c37-4cd9-87d3-e057e80d2777":
+        #     print(f"符合id的游戏为: {item['Name']}")
+        try:
+            for cateitem in item["CategoryIds"]:
+                # 3604de32-66b9-4af3-8363-e48b3a56d5a6 游戏云保存
+                if cateitem['$guid'] == "3604de32-66b9-4af3-8363-e48b3a56d5a6":
+                    count += 1
+                    playnite_list.append(item["Name"])
+                    playnite_list_id.append(item["_id"]["$guid"])
+                    # print(item["_id"]["$guid"])
+                    for name in pc_games:
+                        pc_game_id = item["_id"]["$guid"]
+                        pc_game = name[:-4]
+                        if pc_game_id == pc_game:
+                            # print(item["Name"])
+                            game_list.append(item["Name"])
+        except KeyError:
+            continue
+    print(count)
+    print(len(game_list))
+    print(len(playnite_list))
+
+    diff_list = list(set(playnite_list) - set(game_list))
+    print(f"pg: {diff_list}")
+
+    # 对比百度上的
+    baidu_list_id = []
+    with open("data/baidu_list.txt", 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip('\n')
+            line = line[:-4]
+            line = line[49:]
+            baidu_list_id.append(line)
+    print(baidu_list_id)
+    print(playnite_list_id)
+    diff_list = list(set(baidu_list_id) - set(playnite_list_id))
+    print(diff_list)
 
 
 if __name__ == '__main__':
     # addnewoption()
     modify_save_gui_path()
+    # check_cloud_games()
